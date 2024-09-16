1ce8421de868bec433db41c84f1b7b6926725f96
pichit.d <pichit.dangnhongpan@gmail.com>
2024-09-16 22:46:42 +0700

    push dl

---
+++ ncin.sh
@@ -0,0 +1,11 @@
+sudo apt-get update -y && sudo apt upgrade -y
+
+sudo apt install nala -y && sudo nala fetch
+
+sudo nala install anacron apt-xapian-index blueman bluez bluez-cups bluez-tools brltty brltty-x11 cifs-utils cinnamon cinnamon-common cinnamon-control-center cinnamon-control-center-data cinnamon-core cinnamon-desktop-data cinnamon-session cinnamon-session-common cinnamon-settings-daemon colord cups cups-bsd cups-client cups-filters dbus-x11 dconf-cli evince evince-common fontconfig fontconfig-config foomatic-db-compressed-ppds gdebi ghostscript-x gir1.2-cinnamondesktop-3.0 gir1.2-cmenu-3.0 gir1.2-cvc-1.0 gnome-disk-utility gnome-screenshot gnome-system-monitor gnome-terminal gstreamer1.0-packagekit gstreamer1.0-plugins-base-apps gstreamer1.0-pulseaudio gtk2-engines gtk2-engines-murrine gtk2-engines-pixbuf gvfs-backends hplip ayatana-indicator-printers inxi inputattach keyutils laptop-detect libatk-adaptor libcinnamon-desktop4 libcinnamon-menu-3-0 libcvc0 libgl1-mesa-dri libgsettings-qt1 libgtk2.0-bin libnotify-bin libxcursor1 lm-sensors mesa-utils mousepad mousetweaks nemo nemo-data nemo-fileroller nemo-font-manager numix-gtk-theme numix-icon-theme numix-icon-theme-circle openprinting-ppds p7zip-full pamix pasystray pavucontrol pavumeter polkitd pkexec printer-driver-all profile-sync-daemon pulseaudio pulseaudio-module-bluetooth redshift slick-greeter smbclient software-properties-gtk spice-vdagent system-config-printer system-config-printer-common viewnior x11-apps x11-utils x11-xserver-utils xapps-common xarchiver xbacklight xcursor-themes xdg-user-dirs xdg-user-dirs-gtk xfonts-100dpi xfonts-75dpi xfonts-base xfonts-encodings xfonts-scalable xfonts-utils xorg-docs-core xsensors xserver-xorg xserver-xorg-video-fbdev xauth cmst cbm 7zip pcmanfm galculator gparted grub-customizer mtools mmv zstd lz4 binfmt-support xfonts-thai-nectec libqt5widgets5 libusb-1.0-0-dev ntfs-3g neofetch ghex surf exfatprogs -y
+
+sudo nala install gdm3 -y
+
+sudo nala clean
+
+sudo reboot
---
+++ sgn.sh
@@ -0,0 +1,13 @@
+sudo apt-get update -y && sudo apt upgrade -y
+
+sudo apt install nala -y && sudo nala fetch
+
+sudo apt install gdm3 -y
+
+sudo apt install cmst cbm 7zip pcmanfm ghex galculator gparted grub-customizer mtools mmv zstd lz4 binfmt-support xfonts-thai-nectec libqt5widgets5 libusb-1.0-0-dev ntfs-3g neofetch numix-gtk-theme numix-icon-theme numix-icon-theme-circle gnome-tweaks mousepad mousetweaks nemo nemo-data nemo-fileroller nemo-font-manager viewnior surf exfatprogs -y
+
+sudo apt install anacron apt-xapian-index at-spi2-core colord cups dbus-x11 dmz-cursor-theme dconf-cli eject foomatic-db-compressed-ppds gdebi gnome-calculator gnome-control-center gnome-disk-utility gnome-desktop3-data gnome-keyring gnome-menus gnome-screenshot gnome-disk-utility gnome-system-monitor gnome-terminal gnome-session gnome-shell inputattach libnotify-bin libpulsedsp lm-sensors pavucontrol printer-driver-all profile-sync-daemon pulseaudio pulseaudio-module-bluetooth software-properties-gtk system-config-printer tracker tracker-extract tracker-miner-fs upower x11-apps x11-session-utils x11-utils x11-xserver-utils xarchiver xdg-user-dirs xdg-user-dirs-gtk xfonts-base xserver-xorg xwayland zenity -y
+
+sudo nala clean
+
+sudo reboot
